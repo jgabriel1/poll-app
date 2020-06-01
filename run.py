788018a8d1c0c3f8e257f285dd1c0c80690f1d48
jgabriel1 <josegabriel@det.ufc.br>
2020-06-01 00:02:20 -0300

    Deleted most of the old project files. Kept tests for future reference.

--- run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.views import *
-from config import DevelopmentConfig
-
-if __name__ == "__main__":
-    config_object = DevelopmentConfig()
-    
-    app = create_app(config_object)
-    app.run(debug=True)
